7437a6abec839df02ec9bfb63cff73c12cde0a2d
Thomas Elliott <tom.w.elliott@gmail.com>
2016-02-15 14:36:19 -0500

    Use the message id rather than time to determine that we only get messages we haven't dealt with before. Iterates over all available messages at startup to identify our starting point. This may be slightly inefficient. Update travis yml to use Travis-specified scripts, rather than overriding.

--- Pod/Classes/YXTUnsatisfiableConstraintsDetector.m
+++ Pod/Classes/YXTUnsatisfiableConstraintsDetector.m
@@ -11,7 +11,7 @@
 
 @interface YXTUnsatisfiableConstraintsDetector ()
 
-@property NSDate *lastLogCheck;
+@property NSString *lastLogId;
 @property NSTimer *timer;
 @property NSNotificationCenter *center;
 
@@ -19,9 +19,11 @@
 
 @implementation YXTUnsatisfiableConstraintsDetector
 
+#define MSG_SEQUENCE_KEY ASL_KEY_MSG_ID
+
 - (id) init {
     if(self = [super init]){
-        self.lastLogCheck = [NSDate dateWithTimeIntervalSinceNow:-0.5];
+        [self setLastLog];
         self.pollInterval = 0.5;
         self.center = [NSNotificationCenter defaultCenter];
     }
@@ -45,17 +47,29 @@
     [self.timer invalidate];
 }
 
+- (void)setLastLog
+{
+    aslmsg q, m;
+    
+    q = asl_new(ASL_TYPE_QUERY);
+    aslresponse r = asl_search(NULL, q);
+    while (NULL != (m = asl_next(r)))
+    {
+        const char *time = asl_get(m, MSG_SEQUENCE_KEY);
+        self.lastLogId = [NSString stringWithUTF8String:time];
+    }
+    asl_release(r);
+}
+
+
 - (void)checkForUnsatisfiableConstraints
 {
     aslmsg q, m;
     
     q = asl_new(ASL_TYPE_QUERY);
-    if(self.lastLogCheck != nil){
-        NSString *logSince = [NSString stringWithFormat:@"%.0f", [self.lastLogCheck
-                                                              timeIntervalSince1970]];
-    asl_set_query(q, ASL_KEY_TIME, [logSince UTF8String], ASL_QUERY_OP_GREATER_EQUAL);
+    if(self.lastLogId != nil){
+        asl_set_query(q, MSG_SEQUENCE_KEY, [self.lastLogId UTF8String], ASL_QUERY_OP_GREATER);
     }
-    self.lastLogCheck = [NSDate date];
     
     aslresponse r = asl_search(NULL, q);
     while (NULL != (m = asl_next(r)))
@@ -66,6 +80,8 @@
             // Check this against all views
             [self searchForViolationsForMessage:line];
         }
+        const char *time = asl_get(m, MSG_SEQUENCE_KEY);
+        self.lastLogId = [NSString stringWithUTF8String:time];
     }
     asl_release(r);
 }
